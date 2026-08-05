ad537a28a161bcb9dc9231575ebcd37c83501048
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2026-08-05 03:14:39 +0300

    Add initial implementation of Todo API with CRUD operations and in-memory database

---
+++ todo/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpLogging();
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+    
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseHttpLogging();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
